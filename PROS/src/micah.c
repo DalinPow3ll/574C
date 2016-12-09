bd5c138ea72c4e9c22da48939ca695602d8f121e
micah <micahrassi@gmail.com>
2016-12-09 11:33:27 -0500

    constant power instead of pid

--- PROS/src/micah.c
+++ PROS/src/micah.c
@@ -1,19 +1,16 @@
 #include "API.h"
 
-//variable for mClaw PID
-int position;
-
 void mDrive(){
   //drive code
   int power;
   int turn;
-  power = joystickGetAnalog(1, 1); // vertical axis on left joystick
+  power = -joystickGetAnalog(1, 1); // vertical axis on left joystick
   turn  = joystickGetAnalog(1, 2); // horizontal axis on left joystick
   if(power > 15 || power < -15 || turn > 15 || turn < -15){
     motorSet(2, power - turn); // set left wheels
-    motorSet(9, power - turn); // set left wheels
+    motorSet(9, power + turn); // set left wheels
     motorSet(3, power + turn); // set right
-    motorSet(10, power + turn); // set right
+    motorSet(10, power - turn); // set right
   }else{
     motorStop(2);
     motorStop(3);
@@ -23,22 +20,16 @@ void mDrive(){
 }
 
 void mClaw(){
-  int clawPot = analogRead(2); // set potentiometer value
-
   // claw code
   if(joystickGetDigital(1, 6, JOY_UP)) {
-    motorSet(8, 127); //set motor
-    position = clawPot; // set current position of claw
+    if(analogRead(2) > 100){
+      motorSet(8, 40); // set motor low power
+    }else{
+      motorSet(8, 127); //set motor full power
+    }
   }else if(joystickGetDigital(1, 6, JOY_DOWN)) {
     motorSet(8, -127); // set motor
-    position = clawPot; // set current position of claw
   }else{
-    // if statement for error correction
-    if(clawPot > position){
-      int dif = clawPot - position; // find the difference
-      motorSet(8, 127-(dif/2)); // set the motor to the make up the difference
-    }else{
-      motorSet(8, 0);
-    }
+    motorSet(8, 0);
   }
 }
