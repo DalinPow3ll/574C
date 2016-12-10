3eaf6de1dfa4a5fcea427c3aeae8b44b482a4428
micah <micahrassi@gmail.com>
2016-12-09 22:52:55 -0500

    reorganization

--- PROS/src/micah.c
+++ PROS/src/micah.c
@@ -1,39 +1,31 @@
 #include "API.h"
 
 //drive variables
-int power = 0;
 int turn = 0;
+int power = 0;
 
 void mDrive(){
 
-  int joyP;
   int joyT;
+  int joyP;
 
   int slew = 3; //slew constant
 
-  joyP = -joystickGetAnalog(1, 1); // horizontal axis on left joystick
-  joyT = joystickGetAnalog(1, 2); // vertical axis on left joystick
+
+  joyT = -joystickGetAnalog(1, 1); // horizontal axis on left joystick
+  joyP = joystickGetAnalog(1, 2); // vertical axis on left joystick
 
   //dead zone
-  if(abs(joyP) > 15){ // get the absolute value of joystick on horizontal axis
-    joyP = 0;
-  }
-  if(abs(joyT) > 15 ){ // get the absolute value of joystick on horizontal axis
+  if(abs(joyT) > 15){ // get the absolute value of joystick on horizontal axis
     joyT = 0;
+  }
+  if(abs(joyP) > 15 ){ // get the absolute value of joystick on horizontal axis
+    joyP = 0;
   }
 
 
   //slowly accelerate motors to where joystick is
   //accelerate at rate of slew
-
-  //power
-  if(power < joyP-slew){
-    power += slew;
-  }else if(power > joyP+slew){
-    power -= slew;
-  }else{
-    power = 0;
-  }
 
   //turn
   if(turn < joyT-slew){
@@ -44,22 +36,31 @@ void mDrive(){
     turn = 0;
   }
 
+  //power
+  if(power < joyP-slew){
+    power += slew;
+  }else if(power > joyP+slew){
+    power -= slew;
+  }else{
+    power = 0;
+  }
+
     //left wheels
-    motorSet(9, power + turn);
-    motorSet(3, power + turn);
+    motorSet(9, turn + power);
+    motorSet(3, turn + power);
 
     // right wheels
-    motorSet(10, power - turn);
-    motorSet(2, power - turn);
+    motorSet(10, turn - power);
+    motorSet(2, turn - power);
 }
 
 void mClaw(){
   // claw code
   if(joystickGetDigital(1, 6, JOY_UP)) {
     if(analogRead(2) > 100){
-      motorSet(8, 40); // set motor low power
+      motorSet(8, 40); // set motor low turn
     }else{
-      motorSet(8, 127); //set motor full power
+      motorSet(8, 127); //set motor full turn
     }
   }else if(joystickGetDigital(1, 6, JOY_DOWN)) {
     motorSet(8, -127); // set motor
