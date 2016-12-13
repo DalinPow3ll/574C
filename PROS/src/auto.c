c5e28e90f672ad406010bd0dd62f9f44156ac9b5
micah <micahrassi@gmail.com>
2016-12-13 13:59:34 -0500

    made the autonomous timings much better

--- PROS/src/auto.c
+++ PROS/src/auto.c
@@ -6,34 +6,35 @@
 void autonomous() {
 
   /*drive key
-  0 = forward
-  1 = backward
+  0 = backward
+  1 = forward
   2 = left
   3 = right
   4 = stop
   */
 
-  // basic auto program
+  // basic auto program for right side
   aDrive(1); // forward
   motorSet(claw, 127); // close claw
-  delay(1500);
+  delay(1350);
   aDrive(4); //stop
-  delay(1300);
+  delay(1600);
   motorSet(claw, 40); //low power
   delay(300);
   aLift(-127); //lift the cube
   delay(500);
   aLift(-20); //hold the cube
   aDrive(3); //turn
-  delay(1300);
+  delay(420); //the most dank delay
   aDrive(4); //stop
   delay(300);
   aDrive(0); //reverse
-  delay(400);
-  aLift(-127);
   delay(300);
+  aLift(-127);
   motorSet(claw, -127);
   delay(300);
+  aLift(0); // stop lift
+  delay(600);
   motorStopAll(); //stop everything
   aDrive(4); // stop
 }
