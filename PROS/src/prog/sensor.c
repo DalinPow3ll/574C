d603c7f8342008600bfe4778dddeee42cd2ec166
Marsgate <micahrassi@gmail.com>
2017-01-06 12:25:50 -0500

    skills auto in progress

--- PROS/src/prog/sensor.c
+++ PROS/src/prog/sensor.c
@@ -6,8 +6,9 @@
 int RIGHT90 = -260;
 int LEFT90 = 260;
 
-int RIGHTCUBE = 330;
-int LEFTCUBE = 330;
+int RIGHT45 = -120;
+int LEFT45 = 120;
+
 
 int margin = 10; //encoder turn margin
 
@@ -32,7 +33,7 @@ void rSensor(){
     if(analogRead(clawPot) < cClose){ //if claw not at closed position
       motorSet(claw, 127); //move it forward
     }else{
-      motorSet(claw, 30); //low power
+      motorSet(claw, 0); //stop claw
       toDo--; // task complete
     }
 
@@ -42,12 +43,16 @@ void rSensor(){
       toDo--; // task complete
     }
 
-    //drive forward
-    if (encoderGet(driveEnc) < 820){
-      aDrive(1); //drive forward
-    }else{
-      aDrive(4); // stop
-      toDo--;
+    if(analogRead(clawPot) > cClose){
+      //turn left
+      if (encoderGet(driveEnc) < LEFT45 - margin){
+        aTank(-90, 90);
+      }else if(encoderGet(driveEnc) > LEFT45 + margin){
+        aTank(50, -50);
+      }else{
+        aDrive(4); // stop
+        toDo--;
+      }
     }
 
 
@@ -58,16 +63,75 @@ void rSensor(){
   }
 
 
-  /*
-  //turn to throw
-  if (encoderGet(driveEnc) > RIGHTCUBE + margin){
-    aTank(70,-70);
-  }else if(encoderGet(driveEnc) < RIGHTCUBE - margin){
-    aTank(-50,50); // on over shoot, go back
-  }else{
-    aDrive(4); // stop
-    toDo--;
+  loopClock = 0; //reset loop clock for next loop
+
+  encoderReset(driveEnc); // encoder reset
+
+  //drive and grab loop
+  while(1){
+    toDo = 2; //tasks in this loop
+
+    //claw logic
+    if(loopClock < 1500 && loopClock > 500){
+      motorSet(claw, 100); //squeeze
+    }else{
+      motorSet(claw, 30); //low power
+      toDo--; // task complete
+    }
+
+    //drive forward
+    if (encoderGet(driveEnc) < 1200){
+      aDrive(1);
+    }else{
+      aDrive(4); // stop
+      toDo--;
+    }
+
+    if(toDo == 0){break;} //end loop if all tasks are complete
+
+    delay(20); //wait an interval to allow tip bar to complete
+    loopClock += 20; //add to the total amount of time this loop has been running
   }
-  */
+
+  //lift arm
+  aLift(-127); //lift stars up
+  delay(600);
+  aLift(-30); //hold the stars up
+
+  //reverse
+  while (encoderGet(driveEnc) > 700){
+    aDrive(0);
+  }
+
+
+  encoderReset(driveEnc); // encoder reset
+
+  //slant reverse and throw
+  while (encoderGet(driveEnc) > -1400){
+    aTank(-127,-40); //slant reverse
+  }
+
+  aDrive(0); //reverse
+  aLift(-127); //lift
+  motorSet(claw, -127);// throw
+  delay(500);
+  aDrive(4); //stop bot
+  
+  motorStopAll(); //pause
+
+
+
+  //dummy loop
+  while(1){
+    toDo = 0; //tasks in this loop
+
+    loopClock = 0; //reset loop clock for next loop
+    encoderReset(driveEnc); // encoder reset
+
+    if(toDo == 0){break;} //end loop if all tasks are complete
+
+    delay(20); //wait an interval to allow tip bar to complete
+    loopClock += 20; //add to the total amount of time this loop has been running
+  }
 
 }
--- PROS/src/prog/star.c
+++ PROS/src/prog/star.c
@@ -2,77 +2,85 @@
 
 void rStar(){
   motorSet(tipBar, 127); // bring out tip bar
-  motorSet(claw, 127); // open claw
+  motorSet(claw, 127); // close claw
   delay(1500);
   motorSet(tipBar, 0); // stop tipbar
-  delay(500);
+  delay(300);
   aDrive(3); //turn left
   delay(300);
-  motorSet(claw, 0);
   aDrive(4); //stop bot
   delay(200);
+  motorSet(claw, 50);
   aDrive(1); // drive forward
-  delay(300);
-  motorSet(claw, 127); //close claw
-  delay(700);
+  delay(1400);
+  aDrive(4); //stop
+  delay(200);
   motorStopAll();
   motorSet(claw, 40); // hold stars
   aLift(-127); //lift stars up
-  delay(550);
-  aLift(-30); //hold the stars up
-  aDrive(0); // reverse
-  delay(1800);
-  aDrive(4); // pause
-  delay(300);
-  aDrive(3); // turn left
-  delay(330);
-  aTank(-127,-80); // reverse
-  delay(300);
-  aDrive(0);
-  delay(1200);
-  aLift(-127);
   delay(600);
-  motorSet(claw, -127);
+  aLift(-30); //hold the stars up
+  aDrive(0);
+  delay(700);
+  aTank(-127,-40); //slant reverse
+  delay(1300);
+  aDrive(0);//reverse
+  delay(600);
+  aLift(-127);
   delay(500);
+  motorSet(claw, -127);
+  delay(700);
   aLift(0);
   motorStopAll();
+  delay(500);
+  aLift(80); //bring lift down
+  delay(900);
+  aLift(40); //slower
+  aDrive(1); //forward
+  delay(400);
+  aLift(0);//stop lift
+  aDrive(4);//stop bot
+  motorStopAll();//end routine
 }
 
 
 void lStar(){
   motorSet(tipBar, 127); // bring out tip bar
-  motorSet(claw, 127); // open claw
+  motorSet(claw, 127); // close claw
   delay(1500);
   motorSet(tipBar, 0); // stop tipbar
-  delay(500);
+  delay(300);
   aDrive(2); //turn right
   delay(300);
-  motorSet(claw, 0);
   aDrive(4); //stop bot
   delay(200);
+  motorSet(claw, 50);
   aDrive(1); // drive forward
-  delay(800);
-  motorSet(claw, 127); //close claw
-  delay(600);
+  delay(1400);
+  aDrive(4); //stop
+  delay(200);
   motorStopAll();
   motorSet(claw, 40); // hold stars
   aLift(-127); //lift stars up
-  delay(550);
+  delay(600);
   aLift(-30); //hold the stars up
-  aDrive(0); // reverse
-  delay(1800);
-  aDrive(4); // pause
-  delay(300);
-  aDrive(2); // turn right
-  delay(330);
-  aDrive(4);
-  delay(300);
-  aDrive(0); // reverse
+  aTank(-55,-127); //slant reverse
+  delay(1400);
+  aDrive(0);//reverse
   delay(1200);
   aLift(-127);
-  delay(200);
-  motorSet(claw, -127);
   delay(500);
+  motorSet(claw, -127);
+  delay(700);
   aLift(0);
   motorStopAll();
+  delay(500);
+  aLift(80); //bring lift down
+  delay(900);
+  aLift(40); //slower
+  aDrive(1); //forward
+  delay(400);
+  aLift(0);//stop lift
+  aDrive(4);//stop bot
+  motorStopAll();//end routine
 }
