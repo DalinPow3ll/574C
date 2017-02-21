c2ee02e090ac0d61c4c42a459ec4e6761177a67a
Marsgate <micahrassi@gmail.com>
2017-02-20 20:26:00 -0500

    fixed lcd

--- PROS/src/LCD.c
+++ PROS/src/LCD.c
@@ -1,89 +1,46 @@
 #include "API.h"
 #include "main.h"
-#pragma GCC diagnostic ignored "-Wwrite-strings"
-
-int page = 1;
-int pressed = 0;
 
 void LCDInit(){
   lcdInit(uart1);
   lcdSetBacklight(uart1, true);
 }
 
-void LCDSlap(){
-  lcdClear(uart1);
-  lcdSetText(uart1, 1, "SLAP SLAP SLAP");
-  lcdSetText(uart1, 2, "CLAP CLAP CLAP");
-}
-
-void LCDScreen(){
-  lcdClear(uart1);
-  lcdSetText(uart1, 1, "I am....");
-  lcdSetText(uart1, 2, "Double Zipper!");
-}
-void LCDZip(){
-  lcdClear(uart1);
-  lcdSetText(uart1, 1, "ZIIIIIIIIIIP");
-  lcdSetText(uart1, 2, "ZIIIIIIIIIIP");
-}
-
-
 void LCDBattery(){
   lcdClear(uart1);
   int power = powerLevelMain();
   char str[16];
-  sprintf(str, "%d", power);
-  lcdSetText(uart1, 1,"Power:");
+  sprintf(str, "Main: %d", power);
+  lcdSetText(uart1, 1, str);
+  int backUp = powerLevelBackup();
+  sprintf(str, "9V: %d", backUp);
   lcdSetText(uart1, 2, str);
 }
-void pageReset(){
-  page = 1;
-}
+
 void LCDPot(){
   lcdClear(uart1);
-  int power = analogRead(clawPot);
+  int value = analogRead(clawPot);
   char str[16];
-  sprintf(str, "%d", power);
+  sprintf(str, "%d", value);
   lcdSetText(uart1, 1,"Potentiometer:");
   lcdSetText(uart1, 2, str);
+}
 
+void LCDEnc(){
+  lcdClear(uart1);
+  int ticks = encoderGet(driveEnc);
+  char str[16];
+  sprintf(str, "%d", ticks);
+  lcdSetText(uart1, 1,"Ticks:");
+  lcdSetText(uart1, 2, str);
 }
 
 void LCDTasker(){
-  if(lcdReadButtons(uart1) == 4 && pressed == 0){
-    pressed = 1;
-    page += 1;
-  }else if(lcdReadButtons(uart1) == 1 && pressed == 0){
-    page -= 1;
-    pressed = 1;
-  }else if(lcdReadButtons(uart1) == 0){
-    pressed = 0;
+  if(lcdReadButtons(uart1) == 1){
+    LCDBattery();
+  }else if(lcdReadButtons(uart1) == 2){
+    LCDPot();
+  }else if(lcdReadButtons(uart1) == 4){
+    LCDEnc();
   }
-
-
-  switch(page){
-    case 1:
-      LCDBattery();
-      break;
-
-    case 2:
-      LCDPot();
-      break;
-
-    case 3:
-      LCDSlap();
-      break;
-
-    case 4:
-      LCDZip();
-      break;
-
-    case 5:
-      pageReset();
-      break;
-
-    default:
-      LCDScreen();
-  }
-
 }
